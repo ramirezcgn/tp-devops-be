f40a9f05552aa325507ee7462e97d611fffb9e11
Nicolas Ramirez <ramirezcgn@gmail.com>
2025-09-28 16:07:23 -0300

    fix redis ip6 error

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -4,6 +4,7 @@ const redis = new Redis({
   host: process.env.REDIS_HOST || 'localhost',
   port: parseInt(process.env.REDIS_PORT || '6379', 10),
   // password: process.env.REDIS_PASS, // descomenta si usas autenticación
+  family: 0, // 4 (IPv4) o 6 (IPv6)
 });
 
 export default redis;
